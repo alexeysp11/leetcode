3fdb8806cc60a8bd785da94f53718b9342d395a2
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-08 12:09:26 +0500

    added a function for working with DbgLog table

---
+++ sql/postgresql/FinancialTransactions_Products.f_AddProduct_UseDbgLog.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE FUNCTION f_AddProduct_UseDbgLog
+(
+    aCategoryId integer, 
+    aProductName character varying(50),
+    aPrice numeric(10,2)
+) 
+RETURNS VOID 
+AS $$
+DECLARE 
+	lProductsTableName TEXT;
+    lTransactionName TEXT;
+	lInputParametersString TEXT;
+    lInsertProductStatus TEXT;
+	lErrCode TEXT;
+	lMsgText TEXT;
+	lExcContext TEXT;
+BEGIN
+    lTransactionName = 'f_AddProduct_UseDbgLog';
+	lProductsTableName = 'Products';
+	lInputParametersString = 'Parameters: aCategoryId: ' || aCategoryId 
+		|| ', aProductName: ' || aProductName
+		|| ', aPrice: ' || aPrice;
+
+    INSERT INTO FinancialTransactions_DbgLogs (TransactionName, TransactionDetails, TransactionStatus)
+    VALUES (lTransactionName, 'Begin', 'Started');
+
+	BEGIN
+		IF (SELECT COUNT(*) FROM Products WHERE categoryid = aCategoryId AND productname = aProductName AND price = aPrice) = 0
+		THEN
+			INSERT INTO Products (categoryid, productname, price)
+			VALUES (aCategoryId, aProductName, aPrice);
+		ELSE
+			RAISE EXCEPTION 'The record with the specified parameters already exists in table %. %', 
+				lProductsTableName, lInputParametersString;
+		END IF;
+
+        lInsertProductStatus = 'Record inserted. Table: ' || lProductsTableName 
+			|| '. ' || lInputParametersString;
+	EXCEPTION WHEN OTHERS 
+	THEN
+		GET STACKED DIAGNOSTICS
+		lErrCode = RETURNED_SQLSTATE,
+		lMsgText = MESSAGE_TEXT,
+		lExcContext = PG_CONTEXT;
+
+		lInsertProductStatus = 'ERROR CODE: ' || lErrCode || ' MESSAGE TEXT: ' || lMsgText || ' CONTEXT: ' lExcContext;
+
+   		RAISE NOTICE 'ERROR CODE: % MESSAGE TEXT: % CONTEXT: %', lErrCode, lMsgText, lExcContext;
+  	END;
+
+    INSERT INTO FinancialTransactions_DbgLogs (TransactionName, TransactionDetails, TransactionStatus)
+    VALUES (lTransactionName, lInsertProductStatus, 'Active');
+
+    INSERT INTO FinancialTransactions_DbgLogs (TransactionName, TransactionDetails, TransactionStatus)
+    VALUES (lTransactionName, 'End', 'Finished');
+END;
+$$ LANGUAGE plpgsql;
+
+-- select f_AddProduct_UseDbgLog(1, 'Cap', 10);
