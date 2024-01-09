e3d175fe530bb290b5419f2d292b198065f64bb7
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-09 10:40:53 +0500

    edited binary search tree example

--- cs/trees/ImplementBinarySearchTree.cs
+++ cs/trees/ImplementBinarySearchTree.cs
@@ -1,57 +1,65 @@
 using System.Collections.Generic; 
+using System.Linq; 
 
 namespace Studying.Leetcode.Trees
 {
     /// <summary>
-    /// 
+    /// A class for practicing skills in working with a binary tree (basic operations: initialization, addition, deletion, recalculation).
     /// </summary>
     public class ImplementBinarySearchTree : Studying.Leetcode.ILeetcodeProblem
     {
         /// <summary>
-        /// 
+        /// A tree element presented in general form.
         /// </summary>
         public class TreeElement<T>
         {
             #region Public properties
             /// <summary>
-            /// 
+            /// Tree element value.
             /// </summary>
             public T Value { get; private set; }
+
             /// <summary>
-            /// 
+            /// The parent element of the current tree element.
             /// </summary>
             public TreeElement<T> Parent { get; private set; }
+
             /// <summary>
-            /// 
+            /// A collection of child elements of the current tree element.
             /// </summary>
             public ICollection<TreeElement<T>> Childs { get; private set; }
             #endregion  // Public properties
 
             #region Constructors
             /// <summary>
-            /// 
+            /// A constructor to initialize a tree element with the following parameters: a value.
             /// </summary>
             public TreeElement(T value) : this(value, null)
             {
             }
 
             /// <summary>
-            /// 
+            /// A constructor to initialize a tree element with the following parameters: a value and a collection of child elements. 
             /// </summary>
             public TreeElement(T value, ICollection<TreeElement<T>> childs) : this(value, null, childs)
             {
             }
 
             /// <summary>
-            /// 
+            /// A constructor to initialize a tree element with the following parameters: a value, parent element 
+            /// and a collection of child elements.
             /// </summary>
             public TreeElement(T value, TreeElement<T> parent, ICollection<TreeElement<T>> childs)
             {
                 if (childs == null) 
                     childs = new List<TreeElement<T>>();
+                if (childs.Any(x => x == null))
+                    throw new System.Exception("Specified collection of child elements has an element that is equal to null");
+                
                 foreach (var child in childs)
                 {
-                    child.SetParent(this);
+                    if (child.Parent == null)
+                        child.SetParent(this);
                 }
                 
                 Value = value;
@@ -60,13 +68,17 @@ namespace Studying.Leetcode.Trees
             }
             #endregion  // Constructors
 
+            #region Public methods for a tree structure
+            /// <summary>
+            /// Method for assigning a parent element to the current tree element.
+            /// </summary>
             public void SetParent(TreeElement<T> parent)
             {
-                // 
+                Parent = parent;
             }
 
             /// <summary>
-            /// 
+            /// Method for assigning a child element to the current tree element.
             /// </summary>
             public void AddChild(TreeElement<T> child)
             {
@@ -75,6 +87,7 @@ namespace Studying.Leetcode.Trees
             
                 Childs.Add(child);
             }
+            #endregion  // Public methods for a tree structure
 
             #region ToString methods
             /// <summary>
@@ -83,32 +96,32 @@ namespace Studying.Leetcode.Trees
             /// </summary>
             public override string ToString()
             {
-                return $"[value: {Value}, childs: {ToChildTreeString()}]".Replace(",]", "]"); 
+                return $"[value: {Value}, childs: {ToChildRecursiveString()}]".Replace(",]", "]"); 
             }
             
             /// <summary>
-            /// 
+            /// Method to display all child elements as a string.
             /// </summary>
-            public string ToChildsString()
+            public string ToChildString()
             {
                 string result = "["; 
-                foreach (var element in Childs) 
-                    result += element.Value + ","; 
+                foreach (var child in Childs) 
+                    result += child.Value + ","; 
                 result += "]"; 
                 return result.Replace(",]", "]"); 
             }
 
             /// <summary>
-            /// 
+            /// Method to recursively display all child elements as a string.
             /// </summary>
-            public string ToChildTreeString()
+            public string ToChildRecursiveString()
             {
                 string result = string.Empty; 
-                foreach (var element in Childs) 
+                foreach (var child in Childs) 
                 {
-                    result += "[value: " + element.Value + ", childs: "; 
-                    result += element.Childs.Count == 0 ? element.ToChildsString() : element.ToChildTreeString(); 
-                    result += "],"; 
+                    result += "[value: " + child.Value + ", childs: ";
+                    result += child.Childs.Count == 0 ? child.ToChildString() : child.ToChildRecursiveString();
+                    result += "],";
                 }
                 return result.Replace(",]", "]"); 
             }
@@ -116,47 +129,48 @@ namespace Studying.Leetcode.Trees
         }
 
         /// <summary>
-        /// 
+        /// A class that represents a tree.
         /// </summary>
-        public class BinarySearchTree<T>
+        public class Tree<T>
         {
             #region Public properties
             /// <summary>
-            /// 
+            /// The root element of a tree.
             /// </summary>
             public TreeElement<T> Root { get; private set; }
+
             /// <summary>
-            /// 
+            /// A predicate that imposes a condition on adding an element to the tree.
             /// </summary>
             public System.Predicate<T> AddPredicate { get; private set; }
             #endregion  // Public properties
 
             #region Constructors
             /// <summary>
-            /// 
+            /// Default constructor.
             /// </summary>
-            public BinarySearchTree() : this(null, null)
+            public Tree() : this(null, null)
             {
             }
 
             /// <summary>
-            /// 
+            /// A constructor that takes the root element of a tree as an input parameter.
             /// </summary>
-            public BinarySearchTree(TreeElement<T> root) : this(root, null)
+            public Tree(TreeElement<T> root) : this(root, null)
             {
             }
 
             /// <summary>
-            /// 
+            /// A constructor that takes the predicate as an input parameter.
             /// </summary>
-            public BinarySearchTree(System.Predicate<T> addPredicate) : this(null, addPredicate)
+            public Tree(System.Predicate<T> addPredicate) : this(null, addPredicate)
             {
             }
 
             /// <summary>
-            /// 
+            /// A constructor that takes the root element of the tree and a predicate as input parameters.
             /// </summary>
-            public BinarySearchTree(TreeElement<T> root, System.Predicate<T> addPredicate)
+            public Tree(TreeElement<T> root, System.Predicate<T> addPredicate)
             {
                 Root = root;
                 AddPredicate = addPredicate == null ? (x => true) : addPredicate;
@@ -164,7 +178,7 @@ namespace Studying.Leetcode.Trees
             #endregion  // Constructors
 
             /// <summary>
-            /// 
+            /// Method for adding an element to the tree.
             /// </summary>
             public void AddValue(T value)
             {
@@ -185,23 +199,25 @@ namespace Studying.Leetcode.Trees
                 // - if the value is less, then look at the first child element.
             }
 
+            #region ToString methods
             /// <summary>
-            /// 
+            /// Overridden method for displaying a tree as a string.
             /// </summary>
             public override string ToString()
             {
                 return Root.ToString(); 
             }
+            #endregion  // ToString methods
         }
 
         /// <summary>
-        /// 
+        /// Method to run the example.
         /// </summary>
         public void Execute()
         {
             // Example 1: 
             // input1: [value: 1, childs: [value: 2, childs: [value: 5, childs: []]],[value: 3, childs: []],[value: 4, childs: []]]
-            var tree = new BinarySearchTree<int>(
+            var tree = new Tree<int>(
                 new TreeElement<int>(1, new List<TreeElement<int>> 
                 {
                     new TreeElement<int>(2, new List<TreeElement<int>> { new TreeElement<int>(5)}),
